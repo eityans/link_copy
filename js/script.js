58cf0610a215c1ea826e4d770064cab2b7304694
eityans <eityans@gmail.com>
2019-12-18 12:41:10 +0900

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
-var getPageTitle = document.title;
-var url = location.href;
-console.log(getPageTitle);
-console.log(url);
+var title = document.title;
+var url = location.href+'';
+var text = "["+title+"]("+url+")";
+var textBox = document.createElement("textarea");
+textBox.setAttribute("id", "target");
+textBox.setAttribute("type", "hidden");
+textBox.textContent = text.toString();;
+document.body.appendChild(textBox);
+
+textBox.select();
+document.execCommand('copy');
+document.body.removeChild(textBox);
